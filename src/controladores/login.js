cdc7c6a5f57274a803c6634ab8b12332b742d790
Flávio Peres <flaviopc2@gmail.com>
2023-10-20 16:59:15 -0300

    refatorando tempo do token para 30 minutos

--- src/controladores/login.js
+++ src/controladores/login.js
@@ -21,7 +21,7 @@ const crialogin = async (req, res) => {
             return res.status(404).json({ mensagem: 'Usuário ou senha inválida' })
         }
 
-        const token = jwt.sign({ id: usuarios.id }, process.env.SENHA_JWT, { expiresIn: '2m' })
+        const token = jwt.sign({ id: usuarios.id }, process.env.SENHA_JWT, { expiresIn: '30m' })
 
         const { senha: _, ...dadosUsuario } = usuarios
 
