5bafc77bf332459b8d191c0dc6c90c50cc894bfd
Flávio Peres <flaviopc2@gmail.com>
2023-10-28 10:18:50 -0300

    Mudando as mensagems do schema de cadastro.

--- src/validacoes/schema.js
+++ src/validacoes/schema.js
@@ -3,11 +3,13 @@ const joi = require('joi')
 // Esquema de validação para os campos plcavalo e plcarreta
 const schemaPlacas = joi.object({
 
-    dataAbreviada: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    dataAbreviada: joi.string().required().messages({
+        'any.required': 'O campo Data é obrigatório',
+        'string.empty': 'O campo Data é obrigatório'
     }),
-    horaAbreviada: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    horaAbreviada: joi.string().required().messages({
+        'any.required': 'O campo Hora é obrigatório',
+        'string.empty': 'O campo Hora é obrigatório'
     }),
 
     cliente: joi.string().max(50).required().messages({
@@ -15,48 +17,60 @@ const schemaPlacas = joi.object({
         'string.empty': 'O campo Cliente é obrigatório'
     }),
 
-    quantidade: joi.number().max(50).positive().messages({
-        'number.positive': 'Tem que ser um valor maior que zero.'
+    quantidade: joi.number().max(50).positive().required().messages({
+        'number.positive': 'Tem que ser um valor maior que zero.',
+        'any.required': 'O campo Quantidade é obrigatório',
+        'string.empty': 'O campo Quantidade é obrigatório'
     }),
 
-    di: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    di: joi.string().required().messages({
+        'any.required': 'O campo DI é obrigatório',
+        'string.empty': 'O campo DI é obrigatório'
     }),
 
-    dta: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    dta: joi.string().required().messages({
+        'any.required': 'O campo DTA é obrigatório',
+        'string.empty': 'O campo DTA é obrigatório'
     }),
 
-    tipo_de_carga: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    tipo_de_carga: joi.string().required().messages({
+        'any.required': 'O campo Tipo de Carga é obrigatório',
+        'string.empty': 'O campo Tipo de Carga é obrigatório'
     }),
 
-    processo: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    processo: joi.string().required().messages({
+        'any.required': 'O campo Processo é obrigatório',
+        'string.empty': 'O campo Processo é obrigatório'
     }),
 
-    motorista: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    motorista: joi.string().required().messages({
+        'any.required': 'O campo Motorista é obrigatório',
+        'string.empty': 'O campo Motorista é obrigatório'
     }),
 
-    origem: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    origem: joi.string().required().messages({
+        'any.required': 'O campo Origem é obrigatório',
+        'string.empty': 'O campo Origem é obrigatório'
     }),
 
-    destino: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    destino: joi.string().required().messages({
+        'any.required': 'O campo Destino é obrigatório',
+        'string.empty': 'O campo Destino é obrigatório'
     }),
 
-    ajudantes: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    ajudantes: joi.string().required().messages({
+        'any.required': 'O campo Ajudantes é obrigatório',
+        'string.empty': 'O campo Ajudantes é obrigatório'
     }),
 
-    conferente: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    conferente: joi.string().required().messages({
+        'any.required': 'O campo Conferente é obrigatório',
+        'string.empty': 'O campo Conferente é obrigatório'
     }),
 
-    status: joi.string().max(50).messages({
-        'string.max': 'O Campo Placa Cavalo tem que ter no máximo 7 caracteres'
+    status: joi.string().required().messages({
+        'any.required': 'O campo Status é obrigatório',
+        'string.empty': 'O campo Status é obrigatório'
     }),
 
     pl_cavalo: joi.string().max(7).messages({
