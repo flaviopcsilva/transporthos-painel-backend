6313d37b60ffde6a193efa5da51848d24fa3350f
Flávio Peres <flaviopc2@gmail.com>
2024-03-11 17:57:37 -0300

    redeploy3

--- src/index.js
+++ src/index.js
@@ -31,6 +31,6 @@ app.use(express.json())
 
 app.use(rotas)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT_SERVER, () => {
     console.log(`Servidor Rodando na Porta ${process.env.PORT_SERVER}`)
 })
